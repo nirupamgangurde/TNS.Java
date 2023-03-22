fc1628f15c16d1c35367c04a29b1dcfd34e02ec4
nirupamgangurde <90169731+nirupamgangurde@users.noreply.github.com>
2023-03-22 11:18:16 +0530

    Add files via upload

---
+++ Increment_and_Decrement_Operator.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Increment_and_Decrement_Operator {
+    public static void main(String[] args) {
+//        byte x = 5;
+//        int y = 6;
+//        short z = 8;
+//        int a= y+z;
+//        float b = 6.54f+x;
+//        System.out.println(b);
+//        int a = 50;
+//        System.out.println(a++);//First a is assigned then incremented
+//        System.out.println(a);
+//        System.out.println(++a);//here 1st incremented and then assigned
+//        Character H = 'a';
+//        System.out.println(++H);
+        int x =6;
+        int y = x++;
+        System.out.println(x);
+        System.out.println(y);
+        int a =6;
+        int z =++a;
+        System.out.println(a);
+        System.out.println(z);
+    }
+}
---
+++ Precedence_and_Associativity.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Precedence_and_Associativity {
+    public static void main(String[] args) {
+//        int a = 6*5-34/2;
+//        System.out.println(a);
+        /*
+        =30-34/2
+        =30-17
+        =13
+         */
+        int b = 60/5-34*2;
+        System.out.println(b);
+        /*
+        =12-34*2
+        =12-68
+        =-56
+         */
+    }
+}
