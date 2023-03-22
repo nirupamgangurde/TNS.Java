f35545ae7649dc817eca84d19ffaeb69e790898c
nirupamgangurde <90169731+nirupamgangurde@users.noreply.github.com>
2023-03-22 11:15:58 +0530

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Operators {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 10 + a;
+        System.out.println(b);
+    }
+}
---
+++ Taking_Inputs.java
@@ -0,0 +1,19 @@
+package com.company;
+import java.util.Scanner;
+public class Taking_Inputs {
+    public static void main(String[] args) {
+          System.out.println("It Works!");
+          Scanner scan = new Scanner(System.in);
+//        System.out.println("Enter Number 1");
+//        int a = scan.nextInt();
+//        System.out.println("Enter Number 2");
+//        int b = scan.nextInt();
+//        int sum = a + b;
+//        System.out.println("The sum of these numbers is "+ sum);
+//        System.out.println(sum);
+//        boolean b1 = scan.hasNextInt();
+//        String s1 = scan.next();
+        String s1 =scan.nextLine();
+        System.out.println(s1);
+    }
+}
