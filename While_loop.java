251556e7041b592f0473a25db17cc4b1162f2e04
Nirupam Gangurde <90169731+nirupamgangurde@users.noreply.github.com>
2023-03-29 21:24:52 +0530

    Add files via upload

---
+++ While_loop.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class While_loop {
+    public static void main(String[] args) {
+//        program to print natural numbers from 100 to 200
+        int i = 100;
+        while (i<=200){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
