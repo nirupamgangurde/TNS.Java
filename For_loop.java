18a8a0a28481bc7f368fdb6ef76649ce597c6477
Nirupam Gangurde <90169731+nirupamgangurde@users.noreply.github.com>
2023-03-30 09:16:06 +0530

    Add files via upload

---
+++ For_loop.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class For_loop {
+    public static void main(String[] args) {
+//        for(int i=1;i<=10;i++){
+//            System.out.println(i);
+//        }
+//      CODE FOR EVEN NO.
+//        for(int i = 0; i<=5;i++){
+//            System.out.println(2*i+1);
+//        }
+//        CODE FOR ODD NUMBER
+//        for(int i =0;i<=10;i++){
+//            System.out.println(2*i);
+//        }
+//        for(int i=5;i>0;i--){
+//            System.out.println(i);
+//        }
+        int n =10;
+        for(int i=n;i>0;i--){
+            System.out.println(i);
+        }
+    }
+}
