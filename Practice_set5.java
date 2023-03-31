48538f81f952c5c0d99293030e4410cf46f28620
Nirupam Gangurde <90169731+nirupamgangurde@users.noreply.github.com>
2023-03-31 23:42:36 +0530

    Add files via upload

---
+++ Practice_set5.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class Practice_set5 {
+    public static void main(String[] args) {
+//        int n=4;
+//        for(int i=n;i>0;i--){
+//            for(int j=0;j<i;j++){
+//                System.out.print("*");
+//            }
+//            System.out.print("\n");
+//        }
+//        Question 2
+//        int a =0;
+//        int n =3;
+//        for(int i =0;i<3;i++){
+//            a=a+(2*i);
+//        }
+//        System.out.println("Sum of the even no.is "+ a);
+//        Question 3
+//        int n =5;
+//        for(int i=1;i<=10;i++){
+//            System.out.printf("%d * %d = %d\n",n,i,n*i);
+//        }
+//        QUESTION 4
+//        int n =10;
+//        for(int i =10;i>=1;i--){
+//            System.out.printf("%d * %d = %d\n",n,i,n*i);
+//        }
+        
+   }
+
+}
